7cca3eee8d339027cab0e6b61bd166e97df68b39
Marcos Molina <marcosmol204@gmail.com>
2022-12-16 15:19:41 +0200

    added priter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,23 @@
 module.exports = {
   env: {
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: 'standard-with-typescript',
-  overrides: [
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json']
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json"],
   },
   rules: {
-  }
-}
+    "no-console": 1,
+    "prettier/prettier": 2,
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export const sum = (a: number, c: number): number => a + c
+export const sum = (a: number, c: number): number => a + c;
